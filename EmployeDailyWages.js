622fa616641dc7754b63efaef4963da3072e3aa0
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-07-21 17:33:55 +0530

    Added: Calculating the Total hours and Monthly Wage

--- EmployeDailyWages.js
+++ EmployeDailyWages.js
@@ -3,7 +3,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const WAGE_PER_HOURS=20;
 const FULL_TIME_HOURS=8;
-
+const NUM_OF_WORKING_DAYS=20;
  function getWorkingHours(empCheck1){
     switch(empCheck1){
         case IS_PART_TIME:
@@ -14,8 +14,13 @@ const FULL_TIME_HOURS=8;
             return 0;    
     }
 }
+
 let empHrs=0;
- empCheck1=Math.floor(Math.random()*10)%3;
- empHrs=getWorkingHours(empCheck1)
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+    empCheck1=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck1);
+
+}
+
 let empWage=empHrs*WAGE_PER_HOURS;
-console.log("Emp Wage: "+empWage)
+console.log("Total hrs: "+empHrs+", Emp Wage: "+empWage)
