4c4e39a5bd5edd5022d89fceca37dc0e9e461fcc
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-07-21 17:17:37 +0530

    Added: Calculating the Employee Wage

--- EmployeDailyWages.js
+++ EmployeDailyWages.js
@@ -1,10 +1,19 @@
-const IS_ABSENT=0;
-
-let empCheck=Math.floor(Math.random()*10)%3;
-
-if(empCheck==IS_ABSENT){
-    console.log("Employee is Absent");
-    return;
-}else{
-    console.log("Employee is present");
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const WAGE_PER_HOURS=20;
+const FULL_TIME_HOURS=8;
+let empHrs=0;
+ empCheck1=Math.floor(Math.random()*10)%3;
+switch(empCheck1){
+    case IS_PART_TIME:
+        empHrs= PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs= FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs= 0;    
 }
+let empWage=empHrs*WAGE_PER_HOURS;
+console.log("Emp Wage: "+empWage)
